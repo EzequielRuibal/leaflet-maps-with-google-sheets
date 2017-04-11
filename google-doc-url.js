587d2b6d7dbf1078c458833e342010d77a9352a7
EzequielRuibal <eze1965@gmail.com>
2017-04-12 01:15:41 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://drive.google.com/open?id=1IAIhybxA3dnpO_7DQgcWWA5zvAVgHaQFQCCMHNQXNhY/edit#gid=0';
